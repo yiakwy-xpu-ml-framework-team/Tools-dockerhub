00d52dd3cfa6222412da318e0ca753d4cd58ed65
yiakwy-xpu-ml-framework-team <89890040+yiakwy-xpu-ml-framework-team@users.noreply.github.com>
2024-09-02 20:19:22 +0800

    Add rocm (#2)
    * add script to update sdk to update sdk if needed

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Dockerhub
 
 Containts one stop of common environment prepareation for CUDA/ROCM by composing necessary dockerfiles with software SDK (CUDA/CUDNN/CuOpt/ROCM)
+
+# Usage
+
+## ROCM
+
+To update SDK (defaults to /opt/rocm) to a specific version, execute the following scripts:
+
+> ROCM_VERSION=6.3 bash rocm/update_sdk.sh
+
+## CUDA
---
+++ rocm/update_sdk.sh
@@ -0,0 +1,33 @@
+# details can be found in https://rocm.docs.amd.com/projects/install-on-linux/en/latest/install/native-install/ubuntu.html#installing
+
+ROCM_VERSION=${ROCM_VERSION:-6.2}
+
+# Make the directory if it doesn't exist yet.
+# This location is recommended by the distribution maintainers.
+SIGNED_KEY_SAVE_DEST=/etc/apt/trusted.gpg.d # /etc/apt/keyrings
+sudo mkdir --parents --mode=0755 $SIGNED_KEY_SAVE_DEST
+
+# Add rocm repository
+
+# Download the key, convert the signing-key to a full
+# keyring required by apt and store in the keyring directory
+if [ -d $SIGNED_KEY_SAVE_DEST ]; then
+wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | \
+    gpg --dearmor | sudo tee $SIGNED_KEY_SAVE_DEST/rocm.gpg > /dev/null
+else
+wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | apt-key add -
+fi
+
+# register kernel-mode driver
+echo "deb [arch=amd64 signed-by=$SIGNED_KEY_SAVE_DEST/rocm.gpg] https://repo.radeon.com/amdgpu/${ROCM_VERSION}/ubuntu jammy main" \
+    | sudo tee /etc/apt/sources.list.d/amdgpu.list
+sudo apt update
+
+# add the ROCm repository.
+echo "deb [arch=amd64 signed-by=$SIGNED_KEY_SAVE_DEST/rocm.gpg] https://repo.radeon.com/rocm/apt/${ROCM_VERSION} jammy main" \
+    | sudo tee --append /etc/apt/sources.list.d/rocm.list
+echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
+    | sudo tee /etc/apt/preferences.d/rocm-pin-600
+sudo apt update
+
+sudo apt install -y rocm
